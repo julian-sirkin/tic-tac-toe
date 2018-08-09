88966848fa3b76048dbe4cb17b9f949b48b980be
juliansirkin <juliansirkin@gmail.com>
2018-08-09 15:40:38 -0400

    Refactor game logic
    -Add function the check for tie
    -Make Move now named function, no longer anonymous
    -Create variable "over" for game over

--- assets/scripts/gameplay/game-logic.js
+++ assets/scripts/gameplay/game-logic.js
@@ -1,9 +1,9 @@
-// Board defined
-
+// Global variables for game logic
+const store = require('../store.js')
 let gameBoard = ['', '', '', '', '', '', '', '', '']
 const playerOne = 'X'
 const playerTwo = 'O'
-
+let over = false
 let activePlayer = playerOne
 const winningSpaces = [
   [0, 1, 2],
@@ -15,51 +15,66 @@ const winningSpaces = [
   [0, 4, 8],
   [2, 4, 6]
 ]
-
-
-// Marks board with X or O and marks the javascript
-// Game board with X or O at same index
-
+/* Conroller, Updates gameboard array, checks for win,
+checks for tie, changes player, updates if the game
+is over */
 const makeMove = function (space) {
   if (gameBoard[space] === '') {
     gameBoard[space] = activePlayer
-    checkForWin() ? alert('Winner Winner Chicken Dinner') : changePlayer()
+    // checkForWin() ? alert('Winner Winner Chicken Dinner') : changePlayer()
+    gameOver()
+    changePlayer()
   } else {
-  alert('Pick another space')
-    }
-    console.log(gameBoard)
+    alert('Pick another space')
   }
-
-
-
+  console.log(gameBoard)
+  console.log('is game over?', over)
+}
+// Check for winner, check for tie
+const gameOver = function () {
+  if (checkForWin()) {
+    return alert('winner winner chicken dinner')
+  } else if (gameTied(gameBoard)) {
+    return alert('the game has been tied')
+  }
+}
+// Tied Game
+const gameTied = function (gameBoard) {
+  for (let i = 0; i < gameBoard.length; i++) {
+    if (gameBoard[i] === '') {
+      over = false
+      return false
+    }
+  }
+  over = true
+  return true
+}
+// Check for win.....
+const checkForWin = function () {
+for (let i = 0; i < winningSpaces.length; i++) {
+    if (checkPlayer(winningSpaces[i])) {
+      return true
+    }
+  }
+  return false
+}
+// callback in checking for the win
+const checkPlayer = (combo) => {
+  for (let i = 0; i < combo.length; i++) {
+    if (gameBoard[combo[i]] !== activePlayer) {
+      return false
+    }
+  }
+  return true
+}
 // Switch Player
 const changePlayer = function () {
   if (activePlayer === playerOne) {
     activePlayer = playerTwo
-  } else if ( activePlayer === playerTwo) {
+  } else if (activePlayer === playerTwo) {
     activePlayer = playerOne
   }
 }
-
-// Check for win.....
-const checkForWin = function () {
-for (var i = 0; i < winningSpaces.length; i++) {
-  if (checkPlayer(winningSpaces[i])) {
-    return true
-    }
-  }
-  return false
-}
-// callback
-const checkPlayer = (combo) => {
-  for (var i = 0; i < combo.length; i++) {
-    if (gameBoard[combo[i]] !== activePlayer) {
-  return false
-      }
-  }
-  return true
-}
-
 module.exports = {
-makeMove
+  makeMove
 }
